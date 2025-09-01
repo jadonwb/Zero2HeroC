184bfb2ff8c4ea6ba78f9e3b97b538614ec71019
Jadon Brutcher <jadonwbrutcher@gmail.com>
2025-09-01 09:17:10 -0500

    dbheader

--- src/main.c
+++ src/main.c
@@ -8,29 +8,25 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+	printf("Usage: %s -n -f <database file>\n", argv[0]);
+	printf("\t -n  - create new database file\n");
+	printf("\t -f  - (required) path to database file\n");
 }
 
 int main(int argc, char *argv[]) { 
 	char *filepath = NULL;
-	char *portarg = NULL;
-	unsigned short port = 0;
 	bool newfile = false;
-	bool list = false;
 	int c;
+	int dbfd = -1;
+	struct dbheader_t *header = NULL;
 
-	while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
+	while ((c = getopt(argc, argv, "nf:")) != -1) {
 		switch (c) {
 			case 'n':
 				newfile = true;
 				break;
 			case 'f':
 				filepath = optarg;
-				break;
-			case 'p':
-				portarg = optarg;
-				break;
-			case 'l':
-				list = true;
 				break;
 			case '?':
 				printf("Unknown option -%c\n", c);
@@ -40,4 +36,36 @@ int main(int argc, char *argv[]) {
 
 		}
 	}
+
+	if (filepath == NULL) {
+		printf("Filepath is a required argument\n");
+		print_usage(argv);
+		return 0;
+	}
+
+	if (newfile) {
+		dbfd = create_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to create database file\n");
+			return -1;
+		}
+
+		if (create_db_header(dbfd, &header) == STATUS_ERROR) {
+			printf("Failed to create database header\n");
+			return -1;
+		}
+	} else {
+		dbfd = open_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to open database file\n");
+			return -1;
+		}
+
+		if (validate_db_header(dbfd, &header) == STATUS_ERROR) {
+			printf("Failed to validate database header\n");
+			return -1;
+		}
+	}
+
+	output_file(dbfd, header, NULL);
 }
--- src/parse.c
+++ src/parse.c
@@ -23,15 +23,84 @@ int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employe
 }
 
 int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
+    if (fd < 0) {
+        printf("Invalid file descriptor\n");
+        return STATUS_ERROR;
+    }
 
+    dbhdr->magic = htonl(dbhdr->magic);
+    dbhdr->filesize = htonl(dbhdr->filesize);
+    dbhdr->count = htons(dbhdr->count);
+    dbhdr->version = htons(dbhdr->version);
+
+    lseek(fd, 0, SEEK_SET);
+
+    write(fd, dbhdr, sizeof(struct dbheader_t));
+    return 0;
 }	
 
 int validate_db_header(int fd, struct dbheader_t **headerOut) {
+    if (fd < 0) {
+        printf("Invalid file descriptor\n");
+        return STATUS_ERROR;
+    }
+
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL) {
+        printf("Malloc failed to create db header\n");
+        return STATUS_ERROR;
+    }
+
+    if (read(fd, header, sizeof(struct dbheader_t)) != sizeof(struct dbheader_t)) {
+        perror("read");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    header->version = ntohs(header->version);
+    header->count = ntohs(header->count);
+    header->magic = ntohl(header->magic);
+    header->filesize = ntohl(header->filesize);
+
+    if (header->magic != HEADER_MAGIC) {
+        printf("Improper header magic\n");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    if (header->version != 0x1) {
+        printf("Improper header version\n");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    struct stat dbstat = {0};
+    fstat(fd, &dbstat);
+    if (header->filesize != dbstat.st_size) {
+        printf("Corrupted database\n");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    *headerOut = header;
+    return STATUS_SUCCESS;
 
 }
 
 int create_db_header(int fd, struct dbheader_t **headerOut) {
-	
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL) {
+        printf("Malloc failed to create db header\n");
+        return STATUS_ERROR;
+    }
+
+    header->version = 0x1;
+    header->count = 0;
+    header->magic = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
+
+    *headerOut = header;
+    return STATUS_SUCCESS;
 }
 
 
